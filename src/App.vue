64530ef158b71756b1c1c4e3d66816a7c173e79c
Qymh <415570450@qq.com>
2017-12-04 20:25:24 +0800

    2017.12.4

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,28 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  mounted () {
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?44321327b5eeecd5eeb732a556967e98";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
+    (function(){    
+      var bp = document.createElement('script');
+      var curProtocol = window.location.protocol.split(':')[0];
+      if (curProtocol === 'https') {
+          bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+      }
+      else {
+          bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+      }
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(bp, s);
+    })();
+  }
 }
 </script>
 
